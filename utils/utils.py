f35a07d39e8c3cbc73dbffee9f573ad890672dcf
Administrator <uiui1999vn@gmail.com>
2021-06-08 01:03:33 +0700

    add utils

---
+++ utils/utils.py
@@ -0,0 +1,125 @@
+import math
+import numpy as np
+import pickle
+
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        data = pickle.load(fo, encoding='bytes')
+    return data
+
+
+def savepickle(file_name, data):
+    with open(file_name, 'wb') as f:
+        pickle.dump(data, f)
+
+
+def yes_or_no(question):
+    reply = str(input(question + ' (y/n): ')).lower().strip()
+    if len(reply) == 0:
+        return yes_or_no("Uhhhh... please enter ")
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+    else:
+        return yes_or_no("Uhhhh... please enter ")
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+sigmoid_v = np.vectorize(sigmoid)
+
+
+def sliding_window(image, window_size, step_size):
+    '''
+    This function returns a patch of the input image `image` of size equal
+    to `window_size`. The first image returned top-left co-ordinates (0, 0)
+    and are increment in both x and y directions by the `step_size` supplied.
+    So, the input parameters are -
+    * `image` - Input Image
+    * `window_size` - Size of Sliding Window
+    * `step_size` - Incremented Size of Window
+    The function returns a tuple -
+    (x, y, im_window)
+    where
+    * x is the top-left x co-ordinate
+    * y is the top-left y co-ordinate
+    * im_window is the sliding window image
+    '''
+    for y in range(0, image.shape[0], step_size[1]):
+        for x in range(0, image.shape[1], step_size[0]):
+            yield (x, y, image[y:y + window_size[1], x:x + window_size[0]])
+
+
+def overlapping_area(detection_1, detection_2):
+    '''
+    Function to calculate overlapping area'si
+    `detection_1` and `detection_2` are 2 detections whose area
+    of overlap needs to be found out.
+    Each detection is list in the format ->
+    [x-top-left, y-top-left, confidence-of-detections, width-of-detection, height-of-detection]
+    The function returns a value between 0 and 1,
+    which represents the area of overlap.
+    0 is no overlap and 1 is complete overlap.
+    Area calculated from ->
+    http://math.stackexchange.com/questions/99565/simplest-way-to-calculate-the-intersect-area-of-two-rectangles
+    '''
+    # Calculate the x-y co-ordinates of the
+    # rectangles
+    x1_tl = detection_1[0]
+    x2_tl = detection_2[0]
+    x1_br = detection_1[0] + detection_1[3]
+    x2_br = detection_2[0] + detection_2[3]
+    y1_tl = detection_1[1]
+    y2_tl = detection_2[1]
+    y1_br = detection_1[1] + detection_1[4]
+    y2_br = detection_2[1] + detection_2[4]
+    # Calculate the overlapping Area
+    x_overlap = max(0, min(x1_br, x2_br) - max(x1_tl, x2_tl))
+    y_overlap = max(0, min(y1_br, y2_br) - max(y1_tl, y2_tl))
+    overlap_area = x_overlap * y_overlap
+    area_1 = detection_1[3] * detection_2[4]
+    area_2 = detection_2[3] * detection_2[4]
+    total_area = area_1 + area_2 - overlap_area
+    return overlap_area / float(total_area)
+
+
+def nms(detections, threshold=.5):
+    '''
+    This function performs Non-Maxima Suppression.
+    `detections` consists of a list of detections.
+    Each detection is in the format ->
+    [x-top-left, y-top-left, confidence-of-detections, width-of-detection, height-of-detection]
+    If the area of overlap is greater than the `threshold`,
+    the area with the lower confidence score is removed.
+    The output is a list of detections.
+    '''
+    if len(detections) == 0:
+        return []
+    # Sort the detections based on confidence score
+    detections = sorted(detections, key=lambda detections: detections[2],
+                        reverse=True)
+    # Unique detections will be appended to this list
+    new_detections = []
+    # Append the first detection
+    new_detections.append(detections[0])
+    # Remove the detection from the original list
+    del detections[0]
+    # For each detection, calculate the overlapping area
+    # and if area of overlap is less than the threshold set
+    # for the detections in `new_detections`, append the
+    # detection to `new_detections`.
+    # In either case, remove the detection from `detections` list.
+    for index, detection in enumerate(detections):
+        for new_detection in new_detections:
+            print(overlapping_area(detection, new_detection))
+            if overlapping_area(detection, new_detection) > threshold:
+                del detections[index]
+                break
+        else:
+            new_detections.append(detection)
+            del detections[index]
+    return new_detections
