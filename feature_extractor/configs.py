3a962c37ff730e40a602ea98c2d8952a6ca919c6
Administrator <uiui1999vn@gmail.com>
2021-06-08 00:47:49 +0700

    add feature extractor config

---
+++ feature_extractor/configs.py
@@ -0,0 +1,11 @@
+# HOG config
+
+HOG_CONFIG = {
+    'min_window_size': [128, 128],
+    'step_size': [10, 10],
+    'orientations': 9,
+    'pixel_per_cell': [5, 5],
+    'cell_per_block': [3, 3],
+    'visualize': False,
+    'transform_sqrt': True
+}
